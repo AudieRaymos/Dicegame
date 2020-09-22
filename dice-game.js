50c503bf5064a9a354ece6fae8c44f6930d09038
AudieRaymos <audriannakraymos@gmail.com>
2020-09-21 18:02:12 -0700

    Alert set for player

--- dice-game.js
+++ dice-game.js
@@ -2,4 +2,9 @@ function rollDice () {
     let goldCoins = 0;
     for (let i = 0: i < 10; i++)
        const roll = math.floor(Math.random() * 6) + 1; 
+       alert('You roll a' + roll + '.');
+       if (roll === 1) {
+           alert('Ga,e over, no more rolls!');
+           break;
+       }
 }
